a42923ac05bcd6f84eb0207bc039f7252ea63b3a
Adaamss <aademhamdi@gmail.com>
2026-04-24 10:38:37 +0100

    fix: fix isValidId and isValidAge logic and add missing keys check

--- src/validator/person.validator.ts
+++ src/validator/person.validator.ts
@@ -1,19 +1,72 @@
 import validator from "validator";
-import { Person } from "../types/person.type";
+import { GENDER, Person, TYPE } from "../types/person.type";
+import { personList } from "../data/personData";
 
-export const validatePersonCreation = (p: Person) => {
+export const isValidBody = (body: Person): boolean => {
+  return (
+    isValidAge(body.age) &&
+    isValidGender(body.gender) &&
+    isValidId(body.id) &&
+    isValidName(body.name) &&
+    isValidType(body.type) &&
+    isValidKeys(body) 
+  );
+};
+
+export const isValidKeys = (body: Person): boolean => {
+  for (const key of Object.keys(body)) {
+    if (
+      key !== "id" &&
+      key !== "name" &&
+      key !== "age" &&
+      key !== "gender" &&
+      key !== "type"
+    )
+      return false;
+  }
   if (
-    p.name === undefined ||
-    p.age == null ||
-    !validator.isAlpha(p.name, "en-US", { ignore: " " })
+    body.id == undefined ||
+    body.id == null ||
+    body.name == undefined ||
+    body.name == null ||
+    body.name == "" ||
+    body.age == null ||
+    body.age == undefined ||
+    body.type == null ||
+    body.type == undefined||
+    body.type == null||
+    body.type == undefined
   ) {
     return false;
   }
+
   return true;
 };
 
+export const isValidName = (name: string): boolean => {
+  return validator.isAlpha(name, "en-US", { ignore: " " }) && name.length >= 3;
+};
+
+export const isValidGender = (gender: string): boolean => {
+  return gender === GENDER.male || gender === GENDER.female;
+};
+
+export const isValidType = (type: string): boolean => {
+  return type === TYPE.kid || type === TYPE.men || type === TYPE.women;
+};
+
 export const isValidId = (id: number) => {
-  if (!Number.isInteger(id) || id <= 0) {
+  
+  return Number.isInteger(id) && id > 0;
+};
+
+export const isValidAge = (age: number): boolean => {
+  return Number.isInteger(age) && age > 0 && age <= 150;
+};
+
+export const isExisitingPerson = (id: number): boolean => {
+  const found = personList.find((p) => p.id === id);
+  if (found) {
     return false;
   }
   return true;
